28c0da6ceb0d74a4dbbbc6c5a9c08d1ad69e2659
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-27 00:17:07 +0000

    youview check this update

--- youview/CHECK_THIS.py
+++ youview/CHECK_THIS.py
@@ -8,4 +8,20 @@ class MyInt(object):
         return self.val1 < rhs.val1
 
 max([MyInt(10, 1),  MyInt(5, 1), MyInt(50, 1), MyInt(-5, 1)])
->> MyInt(50, 1)
+# >> MyInt(50, 1)
+
+class Var(object):
+    def __init__(self):
+        self.val = None
+
+    def __lt__(self, value):
+        return self.val < value
+
+
+k = Var()
+k.val = 54
+
+print(k < 5)
+
+k.val = 1
+print(k < 5)
