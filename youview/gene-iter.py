925cefb2354bc3182e5b81dadf70e42ed9faa004
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-27 02:16:12 +0000

    updated iter gener

--- youview/gene-iter.py
+++ youview/gene-iter.py
@@ -1,9 +1,27 @@
-def do_it():
-    yield 1
-    yield 2
+# def do_it():
+#     yield 1
+#     yield 2
 
-for element in do_it():
-    print(element)
+# for element in do_it():
+#     print(element)
 
 # print(dir(do_it()))
 # print(help(do_it()))
+
+###
+
+import copy
+
+def gen_it(list_):
+    for k in list_:
+        yield k * 2
+
+a = gen_it([1,2,3])
+b = gen_it([1,2,3])
+
+for k in a:
+    print(k)
+
+print(b.__next__())
+print(b.__next__())
+print(b.__next__())
