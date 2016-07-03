0a40ad6e6470dcebb7f868b91587ee8b6cc638f4
Daniel Forgacs @P27K <forgacs.daniel@gmail.com>
2016-07-03 17:36:55 +0100

    fuent python update

--- fluentpython/deckofcards.py
+++ fluentpython/deckofcards.py
@@ -1,19 +1,29 @@
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
-class FrenchDesk:
     ranks = [str(n) for n in range(2, 11)] + list('JQLA')
     suits = 'spades diamonds clubs hearts'.split()
 
-    def __init__():
+    def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
-        pass
 
     def __getitem__(self, position):
         return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+print beer_card
+
+deck = FrenchDeck()
+print len(deck)
+
+print deck[0]
+
+print choice(deck)
