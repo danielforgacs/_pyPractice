e1115b063f681576d92370c828e3bafcd27c7d06
Danielf @P27K <forgacs.daniel@gmail.com>
2016-11-23 12:32:10 +0000

    Updated Qt Standardmodel example with get selection method.

--- Qt2/itemview.py
+++ Qt2/itemview.py
@@ -3,7 +3,7 @@ from PyQt4 import QtGui
 from PyQt4 import QtCore
 
 
-data = ['budapest', 'london', 'rome', 'paris']
+data = ['budapest', 'london', 'rome', 'paris', 'marokko', 'kenya']
 
 
 class Model(QtGui.QStandardItemModel):
@@ -18,15 +18,22 @@ class TableView(QtGui.QTableView):
     def __init__(self, parent=None):
         super(TableView, self).__init__(parent)
         self.model = Model()
+        self.setSortingEnabled(True)
         self.setModel(self.model)
-        # self.QAbstractItemView.NoEditTriggers()
         self.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.selectionModel().selectionChanged.connect(self.get_selection)
+
+    def get_selection(self):
+        indexes = self.selectionModel().selectedIndexes()
+        items = [str(index.data().toPyObject()) for index in indexes]
+
+        print items
 
 
 class MainWindow(QtGui.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setGeometry(50, 100, 400, 800)
+        self.setGeometry(800, 100, 400, 800)
         self.table = TableView(self)
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
