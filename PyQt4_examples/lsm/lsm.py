98883428978ba7f5d9a8276e480c0ddc73163689
Danielf @P27K <forgacs.daniel@gmail.com>
2016-12-13 22:18:46 +0000

    refactored lsm.

--- PyQt4_examples/lsm/lsm.py
+++ PyQt4_examples/lsm/lsm.py
@@ -1,6 +1,13 @@
-test_data = list('abcdefgh')
+test_data = zip(list('abcdefgh'), list('qwertypo'))
 
 
 def get_data():
     for element in test_data:
         yield element
+
+
+data = get_data()
+
+print next(data)
+print next(data)
+print next(data)
