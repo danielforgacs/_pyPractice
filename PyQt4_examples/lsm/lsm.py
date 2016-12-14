f54de2ba426a6ca106d1344aac1309e41f099b72
Danielf @P27K <forgacs.daniel@gmail.com>
2016-12-14 00:16:44 +0000

    updated lsm

--- PyQt4_examples/lsm/lsm.py
+++ PyQt4_examples/lsm/lsm.py
@@ -1,13 +1,15 @@
 test_data = zip(list('abcdefgh'), list('qwertypo'))
+test_data = (
+    'Lorem ipsum dolor sit amet consectetur adipiscing'
+    ' elit Maecenas id massa id velit maximus bibendum'
+    # ' Nunc eu sem ut justo pellentesque volutpat sit'
+    # ' amet eget mi Donec turpis dui mattis ac vestibulum'
+    # ' eu malesuada id tellus Quisque non lacinia justo'
+    # ' et scelerisque nisi Cras semper erat non tincidunt varius'
+    )
+db = test_data.split()
 
 
 def get_data():
-    for element in test_data:
-        yield element
-
-
-data = get_data()
-
-print next(data)
-print next(data)
-print next(data)
+    for element in db:
+        yield element, str(len(element))
