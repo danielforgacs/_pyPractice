20c5de1b0350265109fb31ea8e9b513d6913beae
Danielf @P27K <forgacs.daniel@gmail.com>
2017-01-21 18:27:37 +0000

    Started dynamic module path config.

--- no_grab/zed/pipeline/siteconfig/pathconfig.py
+++ no_grab/zed/pipeline/siteconfig/pathconfig.py
@@ -6,7 +6,23 @@ best option: add to PYTHONPATH
 """
 
 import os
+# from pprint import pprint
+
+def listprint(list_):
+    print '\t'+'\n\t'.join((sorted(list_)))
+
+print '--> default paths:'
+listprint(os.sys.path)
 
 deployroot = 'd:/dev/pyLearn/no_grab/zed/pipeline/library'
-databasetools = 'd:/dev/pyLearn/no_grab/zed/pipeline/library/backend'
-os.sys.path.append(databasetools)
+# databasetools = 'd:/dev/pyLearn/no_grab/zed/pipeline/library/backend'
+# os.sys.path.append(databasetools)
+modules = {'databasetools': 'backend'}
+paths = []
+
+for module in modules:
+    root = deployroot
+    modulepath = os.path.join(root, modules[module])
+    paths.append(module)
+
+os.sys.path.extend(paths)
