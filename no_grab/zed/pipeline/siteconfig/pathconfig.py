5045d42480f6402804dba4db57c6c9e9c380fede
Danielf @P27K <forgacs.daniel@gmail.com>
2017-01-21 19:36:03 +0000

    Implemented dev and deployed imports in pathconfig module.

--- no_grab/zed/pipeline/siteconfig/pathconfig.py
+++ no_grab/zed/pipeline/siteconfig/pathconfig.py
@@ -24,8 +24,19 @@ if devroot:
     os.sys.path.insert(0, devroot)
     print '--> devroot is set:', devroot
 
+    try:
+        import devcon
+    except:
+        devcon = None
+
 for module in modules:
     root = deployroot
+
+    if devcon and hasattr(devcon, 'devimports'):
+        if module in devcon.devimports:
+            print '--> !!! Module is imported as dev:', module
+            root = devroot
+
     modulepath = os.path.join(root, modules[module])
     paths.append(modulepath)
 
