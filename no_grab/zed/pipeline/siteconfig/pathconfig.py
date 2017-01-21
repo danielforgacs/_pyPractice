e32d8f87698dd6aae7bb74fb351c459b32708a31
Danielf @P27K <forgacs.daniel@gmail.com>
2017-01-21 17:56:02 +0000

    updated pathconfig with docstring and static paths for now.

--- no_grab/zed/pipeline/siteconfig/pathconfig.py
+++ no_grab/zed/pipeline/siteconfig/pathconfig.py
@@ -1,1 +1,10 @@
+"""
+siteconfig path must be always visible
+from anywhere.
+"""
+
 import os
+
+deployedpath = ''
+databasetools = 'd:/dev/pyLearn/no_grab/zed/pipeline/library/backend'
+# os.sys.path.append()
