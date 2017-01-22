31eea1c98978ea38c473ae0c49267ea2d9eb19cc
Danielf @P27K <forgacs.daniel@gmail.com>
2017-01-22 22:24:25 +0000

    Added new dictionary normalizer.

---
+++ normalize_dict/normalize_dict_v01.py
@@ -0,0 +1,56 @@
+from pprint import pprint
+
+db = {
+    '1': {'2': {'3': '4'}}
+}
+
+db = {
+    '1': {'2': {'3': '4'}},
+    '2': {'22': {'33': '44'}},
+    '3': '33'
+}
+
+newdict = {}
+
+if isinstance({'1': {'2': {'3': '4'}}}, dict):
+        key = '1'
+
+        if isinstance({'2': {'3': '4'}}, dict):
+            key = '1_2'
+
+            if isinstance({'3': '4'}, dict):
+                key = '1_2_3'
+
+                if isinstance('4', dict):
+                    pass
+                else:
+                    newdict = {'1_2_3': '4'}
+
+
+def flatter(dict_, basekey='', newdict={}):
+    for key, value in dict_.items():
+        if not basekey:
+            flat_key = key
+        else:
+            flat_key = basekey + '_' + key
+
+        flat_value = value
+        print('\t', key)
+        print('\t', value)
+        print('\t', flat_key)
+
+        if isinstance(value, dict):
+            flatter(value, flat_key)
+        else:
+            newdict[flat_key] = flat_value
+            return newdict
+
+    return newdict
+
+
+
+# pprint(newdict)
+# print
+result = flatter(db)
+print(type(result))
+pprint(result)
