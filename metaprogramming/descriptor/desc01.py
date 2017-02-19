2923cd0e27e54f7283d306e4aa3592dedcb97396
Danielf @P27K <forgacs.daniel@gmail.com>
2017-02-19 21:03:11 +0000

    Added descriptor example.

--- metaprogramming/descriptor/desc01.py
+++ metaprogramming/descriptor/desc01.py
@@ -1,12 +1,12 @@
 class Descriptor(object):
     def __init__(self):
-        pass
+        self.value = 0
 
     def __get__(self, instance, cls):
-        pass
+        return self.value
 
     def __set__(self, instance, value):
-        pass
+        self.value = value
 
     def __delete__(self, instance):
         pass
@@ -14,7 +14,33 @@ class Descriptor(object):
 
 class Strange(object):
     attr = Descriptor()
+    # attr = None
+
+    # def __init__(self):
+    #     self.attr = None
 
 
 
 s = Strange()
+x = Strange()
+print(s.attr)
+print(x.attr)
+x.attr = 3
+print(s.attr)
+print(x.attr)
+# x.attr = -1
+# print(s.attr)
+# print(x.attr)
+# s.attr = 'k'
+# print(s.attr)
+# print(x.attr)
+# # print(Strange)
+# # print(Strange.__dict__['attr'].__dict__)
+# print(Strange.__dict__['attr'].__get__(x, Strange))
+# # print(s)
+# # print(s.attr)
+# # print(type(s))
+# # print(type(s).__dict__)
+# # print(type(s).__dict__['attr'])
+# # print(type(s).__dict__['attr'].__get__(s, type(s)))
+# # print(Strange.__dict__['attr'].__get__(s, Strange))#
