29595dcf4debaf5b04fedb68178d9c9fe50de9a5
danielforgacs <forgacs.daniel@gmail.com>
2017-03-01 00:08:20 +0000

    updated productname example B.

--- metaprogramming/productname_tests/productname_test_B.py
+++ metaprogramming/productname_tests/productname_test_B.py
@@ -14,6 +14,7 @@ class Descriptor(object):
 
 class BadContainer(object):
     def __init__(self):
+        # print(self.__dict__)
         self.bad = set()
 
 class Region(Descriptor):
@@ -26,39 +27,47 @@ class VarName(BadContainer):
     region = Region(name='region')
     year = Year(name='year')
 
+    def __init__(self, name=None, year=None):
+        super().__init__()
+        self.region = name
+        self.year = year
+
 variant = VarName()
 variant_b = VarName()
+print(variant.__dict__)
+# print(VarName.__dict__)
 # print(VarName.__dict__)
 # print(variant.__dict__)
 # print(variant.__dict__)
 # variant.region = 'usa'
 # print(variant.__dict__)
-variant.region = 'ger'
-variant_b.region = 'usa'
-print(variant.__dict__)
-print(variant_b.__dict__)
-# variant_b.region = 'x'
-# variant_b.bad = 999
-# print(variant.region)
 # variant.region = 'ger'
-# print(variant.bad)
-# print(variant_b.region)
-# print(variant_b.bad)
-variant_b.region = 'x'
-print(variant_b.__dict__)
-variant_b.region = 'usa'
-print(variant_b.__dict__)
-
-variant_b.year = 'x'
-print(variant.__dict__)
-print(variant_b.__dict__)
-variant_b.region = 'usa'
-print(variant.__dict__)
-print(variant_b.__dict__)
-
-variant_b.year = '17'
-print(variant.__dict__)
-print(variant_b.__dict__)
-variant_b.region = 'd'
-print(variant.__dict__)
-print(variant_b.__dict__)
+# variant_b.region = 'usa'
+# print(variant.__dict__)
+# print(variant_b.__dict__)
+# # variant_b.region = 'x'
+# # variant_b.bad = 999
+# # print(variant.region)
+# # variant.region = 'ger'
+# # print(variant.bad)
+# # print(variant_b.region)
+# # print(variant_b.bad)
+# variant_b.region = 'x'
+# print(variant_b.__dict__)
+# variant_b.region = 'usa'
+# print(variant_b.__dict__)
+#
+# variant_b.year = 'x'
+# print(variant.__dict__)
+# print(variant_b.__dict__)
+# variant_b.region = 'usa'
+# print(variant.__dict__)
+# print(variant_b.__dict__)
+#
+# variant_b.year = '17'
+# print(variant.__dict__)
+# print(variant_b.__dict__)
+# variant_b.region = 'd'
+# print(variant.__dict__)
+# print(variant_b.__dict__)
+#
