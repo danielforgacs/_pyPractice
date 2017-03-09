685ffa5282c5ef06766819336cb6962f2c8df127
danielforgacs <forgacs.daniel@gmail.com>
2017-03-09 23:58:23 +0000

    added ordered class attr.

---
+++ metaprogramming/ordered_class_attr/ordered_class_attr.py
@@ -0,0 +1,28 @@
+class OrderedDescriptor(object):
+    def __init__(self, attrname):
+        self.attrname = attrname
+        self.isorderset = False
+
+    def __set__(self, instance, value):
+        if not self.isorderset:
+            instance.__dict__['order'].append(self.attrname)
+        instance.__dict__[self.attrname] = value
+
+class VN(object):
+    region = OrderedDescriptor(attrname='region')
+    year = OrderedDescriptor(attrname='year')
+    extra = OrderedDescriptor(attrname='extra')
+
+    def __init__(self, k=1):
+        self.order = []
+        self.region = 'region'
+        self.year = 'year'
+        self.extra = 'extra'
+
+    def pr(self):
+        print self.__dict__.keys()
+        print '_'.join([self.__dict__[k] for k in self.order])
+
+vn = VN()
+# print vn.__dict__
+vn.pr()
