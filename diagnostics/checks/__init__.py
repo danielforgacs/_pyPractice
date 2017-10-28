faeb1beaa4f2b0a11486d8cbed4b0a94ec2aae93
ford @mini <forgacs.daniel@gmail.com>
2017-10-28 23:20:54 +0100

    updated diagnostics framework

--- diagnostics/checks/__init__.py
+++ diagnostics/checks/__init__.py
@@ -1,1 +1,2 @@
 from . import check01
+from . import extrachecks
--- diagnostics/diagrunner.py
+++ diagnostics/diagrunner.py
@@ -2,16 +2,12 @@ import checks
 import types
 import baseclass
 
-print dir()
-
-# for name, item in vars(checks).items():
-#     if isinstance(item, types.ModuleType):
-#         for key, value in vars(item).items():
-#             if not key.startswith('_'):
-#                 print value
-
-
-# modules = [module for module in vars(checks).values()
-#             if isinstance(module, types.ModuleType)]
-# print modules
-
+for name, item in vars(checks).items():
+    if isinstance(item, types.ModuleType):
+        for key, value in vars(item).items():
+            if key.startswith('_'):
+                continue
+            if isinstance(value, types.ModuleType):
+                continue
+            if issubclass(value, baseclass.DiagnosticBase):
+                print value
