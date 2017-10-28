0093bd90796be67b52947c564579ae36f5923cdb
ford @mini <forgacs.daniel@gmail.com>
2017-10-28 23:07:17 +0100

    diagnostics update

--- diagnostics/checks/check01.py
+++ diagnostics/checks/check01.py
@@ -1,2 +1,18 @@
-class check_blabl(object):
+import baseclass
+
+
+class CheckFirst(baseclass.DiagnosticBase):
     pass
+
+
+class CheckSecond(object):
+    pass
+
+
+class CheckSecondExtension(CheckSecond):
+    pass
+
+
+# print issubclass(CheckSecondExtension, CheckSecond)
+# print issubclass(CheckFirst, baseclass.DiagnosticBase)
+# print issubclass(CheckSecond, baseclass.DiagnosticBase)
--- diagnostics/diagrunner.py
+++ diagnostics/diagrunner.py
@@ -1,7 +1,15 @@
 import checks
 import types
+import baseclass
 
 for name, item in vars(checks).items():
     if isinstance(item, types.ModuleType):
         for key, value in vars(item).items():
-            print key, isinstance(key, object)
+            if not key.startswith('_'):
+                pass
+
+
+modules = [module for module in vars(checks).values()
+            if isinstance(module, types.ModuleType)]
+print modules
+
