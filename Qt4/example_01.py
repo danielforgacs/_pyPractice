c784c9bb41e475bf1f69a7e52abb7d0cf91a0acf
ford @VBox P27K <forgacs.daniel@gmail.com>
2018-02-04 14:02:42 +0000

    Fixed subclassing.

--- Qt4/example_01.py
+++ Qt4/example_01.py
@@ -8,53 +8,37 @@ ROOT_DIR = '/home/ford/Documents/dev'
 class RepoModel(QtGui.QStandardItemModel):
     def __init__(self):
         super(RepoModel, self).__init__()
-        self.appendRow(QtGui.QStandardItem('sdkfj'))
-        self.appendRow(QtGui.QStandardItem('sdkfj'))
-        self.appendRow(QtGui.QStandardItem('sdkfj'))
-        self.appendRow(QtGui.QStandardItem('sdkfj'))
-
-
+        self.appendRow(QtGui.QStandardItem('A'))
+        self.appendRow(QtGui.QStandardItem('B'))
+        self.appendRow(QtGui.QStandardItem('C'))
+        self.appendRow(QtGui.QStandardItem('E'))
 
 
 class RepoList(QtGui.QListView):
     def __init__(self):
         super(RepoList, self).__init__()
-        self.setModel = RepoModel()
+        model = RepoModel()
+        self.setModel(model)
 
 
 class MainLayout(QtGui.QVBoxLayout):
     def __init__(self):
         super(MainLayout, self).__init__()
-        repolist = RepoList()
-        self.addWidget(repolist)
-
-
-class MainWindow(QtGui.QWidget):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        layout = MainLayout()
-        self.setLayout(layout)
-        self.show()
+        listview = RepoList()
+        self.addWidget(listview)
 
 
 class MainSimple(QtGui.QWidget):
     def __init__(self):
         super(MainSimple, self).__init__()
         self.show()
-        listview = QtGui.QListView()
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(listview)
-        # model = QtGui.QStandardItemModel()
-        model = RepoModel()
+        layout = MainLayout()
         self.setLayout(layout)
-        listview.setModel(model)
-        model.appendRow(QtGui.QStandardItem('zdkfg'))
 
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    mainwin2 = MainWindow()
     mainwin = MainSimple()
     sys.exit(app.exec_())
 
