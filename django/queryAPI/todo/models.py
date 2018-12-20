62897f9a44d7beb9441c30faed96f6ecdec2cb11
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-12-20 00:27:22 +0000

    added initial model

--- django/queryAPI/todo/models.py
+++ django/queryAPI/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+PRIORITY_CHOICES = [(1, 'High'), (2, 'Low')]
+
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    is_done = models.BooleanField(default=False)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES,
+                                   default=1)
