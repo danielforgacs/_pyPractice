b11b76888524475ac194a5fa12d5093fb7005c91
ford@AcerS1 <forgacs.daniel@gmail.com>
2019-01-13 23:53:33 +0000

    test

--- testing/t001_test.py
+++ testing/t001_test.py
@@ -1,7 +1,7 @@
 import pytest
 import time
 
-WAIT_SECS = 2
+WAIT_SECS = 5
 
 
 @pytest.fixture(scope="module")
@@ -37,6 +37,15 @@ class TestClass_TestStuff:
     def test_cls_04(self, class_fixture):
         assert class_fixture == 6
 
+    def test_cls_05(self, class_fixture):
+        assert class_fixture == 6
+
+    def test_cls_06(self, class_fixture):
+        assert class_fixture == 6
+
+    def test_cls_07(self, class_fixture):
+        assert class_fixture == 6
+
 
 
 
@@ -53,6 +62,12 @@ def test_things_04(module_fixture):
     assert module_fixture == 5
 
 def test_things_05(module_fixture):
+    assert module_fixture == 5
+
+def test_things_06(module_fixture):
+    assert module_fixture == 5
+
+def test_things_07(module_fixture):
     assert module_fixture == 5
 
 
